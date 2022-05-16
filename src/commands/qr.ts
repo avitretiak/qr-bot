781128453e5c96c908e256af7f81971aadab44e4
Avi Tretiak <avitretiak@gmail.com>
2022-05-16 12:56:25 -0300

    Add size customization and general improvements

--- src/commands/qr.ts
+++ src/commands/qr.ts
@@ -7,22 +7,22 @@ const data = {
   description: 'Generates a QR for a given string of text!',
   options: [
     {
-      type: 3,
+      type: 'STRING',
       name: 'string',
       description: 'String you wish to encode into a QR Code',
       required: true,
     },
     {
-      type: 3,
+      type: 'STRING',
       name: 'color',
       description:
         'Foreground color of the generated QR Code, (Examples: "red", "rgb(255, 0, 0)" or "#FF0000")',
       required: false,
     },
     {
-      type: 4,
+      type: 'INTEGER',
       name: 'size',
-      description: '',
+      description: 'Size of each QR module in integer pixels (default is 5)',
       required: false,
     },
   ],
@@ -38,7 +38,7 @@ function transformColor(input: string): Array<number> {
 function generateQR(
   string: string,
   color: '#FFFFFF' | Array<Number>,
-  size = 30
+  size = 5,
 ): typeof MessageAttachment {
   return new MessageAttachment(
     qrImage.image(string, {
@@ -56,13 +56,14 @@ const QR = {
   async execute(interaction: typeof Interaction) {
     const userString = `${interaction.options.getString('string')}`;
     const color = interaction.options.getString('color');
+    const size = interaction.options.getInteger('size') ?? 5;
     if (color && !rgba(color).length) {
       await interaction.reply({
-        content: 'Invalid color entered, double-check the color format.',
+        content: 'Unable to parse color, double-check the color format.',
         ephemeral: true,
       });
     } else {
-      const image = generateQR(userString, transformColor(color));
+      const image = generateQR(userString, transformColor(color), size);
       const message: typeof Message = {
         title: 'Your Generated QR',
         image: {
@@ -73,7 +74,6 @@ const QR = {
             name: 'QR Code String',
             value: userString,
           },
-          { name: '\u200B', value: '\u200B' },
         ],
         timestamp: new Date(),
       };
